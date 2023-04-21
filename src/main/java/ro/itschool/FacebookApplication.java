be434e32459bfdb7967988e64ea36a94fc5d7bcc
ChilomAdrian <chilom.adrian99@yahoo.com>
2023-04-21 15:05:03 +0300

    Initial commit

---
+++ src/main/java/ro/itschool/FacebookApplication.java
@@ -0,0 +1,12 @@
+package ro.itschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class FacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookApplication.class, args);
+	}
+
+}
